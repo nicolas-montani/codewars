7d5f7c45a4d9965c8f8387a1dcc306bada897f29
nicolas-montani <95175152+nicolas-montani@users.noreply.github.com>
2023-03-06 20:01:11 +0100

    boolean to string

---
+++ Exercices/8kyu/Boolean_to_String.java
@@ -0,0 +1,11 @@
+'''
+Description:
+Implement a function which convert the given boolean value into its string representation.
+
+'''
+
+public class BooleanToString {
+  public static String convert(boolean b){
+    return Boolean.toString(b);
+  }
+}
