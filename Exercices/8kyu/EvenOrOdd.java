afb71a558b8de547fea8dfd24a7a52afc8507cb0
nicolas-montani <95175152+nicolas-montani@users.noreply.github.com>
2023-03-11 23:55:38 +0100

    remove!

--- Exercices/8kyu/EvenOrOdd.java
+++ Exercices/8kyu/EvenOrOdd.java
@@ -1,6 +1,6 @@
 //Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
 
-public class EvenOrOdd {
+public class EvenOrOdd{
     public static String even_or_odd(int number) {
         return (number%2 == 0) ? "Even" : "Odd";
     }
---
+++ Exercices/8kyu/RemoveExclamationMarks.java
@@ -0,0 +1,10 @@
+
+// Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+
+
+
+class RemoveExclamationMarks{
+    static String removeExclamationMarks(String s) {
+        return s.replaceAll("!",""); 
+    }
+}
